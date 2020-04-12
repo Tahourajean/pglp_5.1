a77abf53b273c38c723b12733b4a29607acb612e
john <john>
2020-04-12 08:34:36 +0200

    definissions de l'interface DAO et de la fabrique

---
+++ src/main/java/persistence/PersonnelDAO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistence;
+
+import fr.uvsq.Personnel;
+
+/**
+ *
+ * @author USER
+ */
+public class PersonnelDAO  extends DAO<Personnel>{
+
+    @Override
+    public Personnel create(Personnel objet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Personnel update(Personnel objet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Personnel delete(Personnel objet) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
